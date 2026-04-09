a4277c370167d2ed23956bb71fa27e83e8126884
Cassiano2003 <cassianocarvalhodesouza@gmail.com>
2026-04-08 22:49:11 -0400

    Imagens das cartas - Cassiano

--- Assets/Scripts/Craft/ResultadoManager.cs
+++ Assets/Scripts/Craft/ResultadoManager.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 public class ResultadoManager : MonoBehaviour
 {
 
+    [Header("Cartas de Missoes")]
     public Cartas CartaAgua; // Referência à carta de água
     public Cartas CartaSal; // Referência à carta de sal
     public Cartas CartaNitratoPotassio; // Referência à carta de fertilizante
+
+    [Header("Cartas de Combate")]
+    public Cartas CartaAcidoCloridrico; // Referência à carta de ácido clorídrico
+    public Cartas CartaHidroxidoSodio; // Referência à carta de hidróxido de sódio
 
     public Cartas ExibirResultado(string resultado)
     {
@@ -24,6 +29,12 @@ public class ResultadoManager : MonoBehaviour
             case "Nitrato de Potassio":
                 Debug.Log("Você criou nitrato de potássio!");
                 return CartaNitratoPotassio; // Retorna a carta de nitrato de potássio
+            case "Acido Cloridrico":
+                Debug.Log("Você criou ácido clorídrico!");
+                return CartaAcidoCloridrico; // Retorna a carta de ácido clorídrico
+            case "Hidroxido de Sodio":
+                Debug.Log("Você criou hidróxido de sódio!");
+                return CartaHidroxidoSodio; // Retorna a carta de hidróxido de sódio
             default:
                 Debug.Log("Combinação desconhecida.");
                 return null;
